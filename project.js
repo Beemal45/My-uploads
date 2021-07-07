2c94ec6a93902dc47f6b139ccf8fa9b6a0ea1aca
Beemal45 <87033803+Beemal45@users.noreply.github.com>
2021-07-07 10:22:58 -0700

    Add files via upload

--- project.js
+++ project.js
@@ -45,23 +45,23 @@ console.log(lapsCompleted)*/
 let countHere = document.getElementById("count-here")
 let saveEl= document.getElementById("save-el")
 
-console.log(countHere)
+//console.log(countHere)
 
 let count= 0
 function increment() {
     count+= 1
     countHere.innerText= count
     console.log(count)
-}
+    }
+    
 function save() {
     let countDash= count+ " - "
     saveEl.textContent+= " "+ countDash
     countHere.innerText= 0
     count= 0
     console.log(count)
-}
-
-
+    }
+   
 /*let message= "You have three new notifications"
 let username = "per"
 console.log(message + ", " + username + "!")
@@ -82,3 +82,63 @@ let greeting= "Welcome back"
 
 welcomeEl.innerText= greeting+name
 welcomeEl.innerText+= " !" */ // '+=' is a shortcut for adding a value to pre-existent "welcomeEl.innerText" variable.
+
+//recap
+/*let firstName = "Bimal"
+let lastName= " Chalise"
+let fullName= firstName+lastName
+console.log(fullName)*/
+
+/*let name= "Linda"
+let greeting= "Hi there"
+function call() {
+    console.log (greeting+","+" " + name +"!")
+}
+call()*/
+
+/*let myPoints= 3
+function add3Points() {
+    myPoints+= 3
+    }
+function remove1Point() {
+    myPoints+= -1
+    }
+add3Points()
+remove1Point()
+add3Points()
+remove1Point()
+add3Points()
+console.log(myPoints)*/
+
+/*let error= document.getElementById("error")
+    function errorMsg() {
+    error.textContent= "Something went wrong, please try again"    
+    console.log (error)
+}*/
+
+//Calculator
+let num1= 8
+let num2= 2
+document.getElementById("num-el").textContent= num1
+document.getElementById("num2-el").textContent= num2
+let result= document.getElementById("result")
+function add() {
+    let addition= num1 + num2
+    result.textContent= "Result:" +" " + addition
+    console.log(addition)
+}
+function substract() {
+    let substraction= num1 - num2
+    result.textContent= "Result:" + " "+ substraction
+    console.log(substraction)
+}
+function multiply() {
+    let multiplication= num1 * num2
+    result.textContent= "Result:" + " "+ multiplication
+    console.log(multiplication)
+}
+function divide() {
+    let division= num1 / num2
+    result.textContent= "Result:" + " "+ division
+    console.log(division)
+}
